33ff11924516347485edd78c70bc99699b0107c5
João Pedro Evaldt <joao.oliveiraevaldt@gmail.com>
2023-08-28 20:13:24 -0300

    first commit

---
+++ model/EstudanteModel.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/aula04/database/Database.php';
+
+class EstudanteModel {
+    public string $nome;
+    public int $idade;
+    private $database;
+
+    public function __construct()
+    {
+        $this->database = new Database();
+    }
+
+    public function listarModel(): array
+    {
+        $dadosArray = $this->database->executeSelect("SELECT * FROM estudantes");
+        return $dadosArray;
+         
+    }
+    
+    public function salvarModel(string $nome, int $idade)
+    {
+        $sql = "INSERT INTO estudantes (nome, idade) values ('$nome', '$idade')";
+        $this->database->insert($sql);
+
+
+    }
+}
