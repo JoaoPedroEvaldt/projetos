8e897be6f0141eb7b3906cda8bb433b8f06e4f7b
João Pedro Evaldt <joao.oliveiraevaldt@gmail.com>
2023-10-10 19:45:18 -0300

    Atualizando

---
+++ www/Api_project/src/Entity/Book.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+use JsonSerializable;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book implements JsonSerializable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 2000)]
+    private ?string $sinopse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getSinopse(): ?string
+    {
+        return $this->sinopse;
+    }
+
+    public function setSinopse(string $sinopse): static
+    {
+        $this->sinopse = $sinopse;
+
+        return $this;
+    }
+    public function JsonSerialize(): mixed
+    {
+        return[
+            "id" =>$this->getId(),
+        ];
+    }
+}
