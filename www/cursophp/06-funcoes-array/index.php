a031dd9d92806595f7c5417ce5d5c03a1e0eb95a
João Pedro Evaldt <joao.oliveiraevaldt@gmail.com>
2023-10-20 22:29:43 -0300

    Atualizando

---
+++ www/cursophp/06-funcoes-array/index.php
@@ -0,0 +1,21 @@
+<?php
+
+//$nomes = array("Joao", "Pedro", "Renata");
+
+//if (is_array($nomes)) {
+    //echo "é um array";
+//} else {
+    //echo "não é um array";
+//}
+
+
+$nomes = array("Joao", "Pedro", "Renata");
+
+if(in_array("Felipe",$nomes)){
+    echo "é um array";
+} else {
+    echo "não é um array";
+}
+
+
+
